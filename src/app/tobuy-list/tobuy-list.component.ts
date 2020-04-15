cd1424ce1ee73daa3f08f5cba1244ee72f4636e2
giulia-panico <giuliapanico@hotmail.it>
2020-04-15 15:33:39 +0200

    primo commit

---
+++ src/app/tobuy-list/tobuy-list.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormArray, FormControl } from '@angular/forms';
+
+
+
+@Component({
+  selector: 'tobuy-list',
+  templateUrl: './tobuy-list.component.html',
+  styleUrls: ['./tobuy-list.component.css'],
+
+})
+export class TobuyListComponent implements OnInit {
+
+
+
+  items = []
+
+
+  constructor() { }
+
+  onAdd(newItem, myForm) {
+    this.items.push(newItem.value);
+    myForm.reset();
+
+  }
+
+  removeItem(newItem) {
+    let index = this.items.indexOf(newItem);
+    this.items.splice(index, 1);
+
+  }
+
+
+
+
+
+
+  ngOnInit() {
+  }
+
+}
